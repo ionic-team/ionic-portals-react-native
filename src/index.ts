d2fbd3758e94914bd80c3ba78f2dbe104b7b4d05
Steven Sherry <steven.sherry@ionic.io>
2022-05-21 00:01:38 -0500

    fix: Check topic subscribed to before emitting events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { NativeEventEmitter, NativeModules, ViewProps } from 'react-native';
+import { EmitterSubscription, NativeEventEmitter, NativeModules, ViewProps } from 'react-native';
 
 const { IONPortalsPubSub, IONPortalManager } = NativeModules;
 
@@ -12,23 +12,33 @@ export interface Message {
 
 const PortalsPubSub = new NativeEventEmitter(IONPortalsPubSub);
 
-let subscriptionRefDict: any = {};
+const subscriptionMap = new Map<number, EmitterSubscription>();
 
 export const subscribe = async (
   topic: string,
   onMessageReceived: (message: Message) => void
 ): Promise<number> => {
   const subscriptionRef = await IONPortalsPubSub.subscribe(topic);
-  subscriptionRefDict[subscriptionRef] = PortalsPubSub.addListener(
-    'PortalsSubscription',
-    onMessageReceived
-  );
+
+  const subscriber = PortalsPubSub.addListener('PortalsSubscription', (message: Message) => {
+    if (message.topic === topic) {
+      onMessageReceived(message);
+    }
+  });
+
+  subscriptionMap.set(subscriptionRef, subscriber);
+
   return subscriptionRef;
 };
 
 export const unsubscribe = (topic: string, subRef: number) => {
   IONPortalsPubSub.unsubscribe(topic, subRef);
-  subscriptionRefDict[subRef] = null;
+
+  const subscription = subscriptionMap.get(subRef); 
+  if (subscription != undefined) {
+    subscription.remove();
+    subscriptionMap.delete(subRef);
+  }
 };
 
 export const publish = (topic: string, data: any) => {
