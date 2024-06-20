cb5579d5ca6ba903281967a5536e2a4dda2d1910
Trevor Lambert <trevorlambert11@gmail.com>
2024-06-20 16:08:07 -0500

    chore: add web

---
+++ web/src/components/ExploreContainer.tsx
@@ -0,0 +1,54 @@
+import './ExploreContainer.css';
+import { IonButton } from '@ionic/react';
+import { subscribe, publish } from '@ionic/portals';
+import { useEffect, useRef, useState } from 'react';
+import { PluginListenerHandle } from '@capacitor/core';
+
+interface ContainerProps {
+  initialNumber: number
+}
+
+interface Message {
+  data: { message: number };
+  topic: string;
+}
+
+const ExploreContainer: React.FC<ContainerProps> = (props: ContainerProps) => {
+  const subscription = useRef<PluginListenerHandle | null>(null);
+  const [appMessage, setAppMessage] = useState<number>();
+
+  const topic = "button:tapped";
+  const subTopic = "button:received";
+  const message = "tapped";
+
+  useEffect(() => {
+    sub();
+
+    return () => {
+      subscription.current?.remove()
+    };
+  }, [])
+
+  const sub = async () => {
+    subscription.current = await subscribe(
+      subTopic,
+      (result: Message) => {
+        console.log(`Got message from ReactNative ${JSON.stringify(result)}`)
+        setAppMessage(result.data.message);
+      }
+    );
+  };
+
+  const pub = async () => {
+    publish({ topic, data: { message } });
+  };
+
+  return (
+    <div className="container">
+      <strong>Hello from Ionic Portals</strong>
+      <IonButton onClick={async () => await pub()}>Next number on tap will be {appMessage === undefined ? props.initialNumber : appMessage}</IonButton>
+    </div>
+  );
+};
+
+export default ExploreContainer;
