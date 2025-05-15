0b02e66d80ecd7a24e844e1db9fe83db9242dce1
Joey Pender <joey.pender@outsystems.com>
2025-05-15 13:16:02 -0500

    fix(android): using correct topic name for storing subscriptions (#67)

--- android/src/main/java/io/ionic/portals/reactnative/ReactNativePortalsPubSub.kt
+++ android/src/main/java/io/ionic/portals/reactnative/ReactNativePortalsPubSub.kt
@@ -27,7 +27,7 @@ internal class PortalsPubSubModule(reactContext: ReactApplicationContext) :
                 .emit(eventName, result.toJSObject().toReactMap())
         }
 
-        subscriptionRefs[eventName] = ref
+        subscriptionRefs[topic] = ref
     }
 
     @ReactMethod
